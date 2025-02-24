a7c793cfab5abe8c1609fb86c3b212aacf98021b
leekery <pashokmoreplay@gmail.com>
2025-02-25 02:56:40 +0300

    clear cache ability

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-// main.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron');
 const path = require('path');
+
+// Флаги для управления очисткой данных
+const shouldClearCache = false;
+const shouldClearStorageData = false;
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -17,7 +20,29 @@ function createWindow() {
     // win.webContents.openDevTools(); // Отладка
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
+    // Очистка кеша, если флаг установлен в true
+    if (shouldClearCache) {
+        try {
+            await session.defaultSession.clearCache();
+            console.log('Cache cleared successfully!');
+        } catch (error) {
+            console.error('Error clearing cache:', error);
+        }
+    }
+    
+    // Очистка прочих данных, если флаг установлен в true
+    if (shouldClearStorageData) {
+        try {
+            await session.defaultSession.clearStorageData({
+                storages: ['appcache', 'cookies', 'localstorage', 'indexdb', 'serviceworkers']
+            });
+            console.log('Another data cleared successfully!');
+        } catch (error) {
+            console.error('Error clearing other data:', error);
+        }
+    }
+
     createWindow();
     
     app.on('activate', () => {
